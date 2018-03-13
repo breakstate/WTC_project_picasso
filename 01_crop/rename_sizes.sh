e6f97d3ba87bbb3004d16fdfe85869ec9a321029
Brenton Moodley <brenton@brenton.wethinkcode.co.za>
2018-03-13 15:51:07 +0200

    mvp, crops renames and sorts in folders

--- 01_crop/rename_sizes.sh
+++ 01_crop/rename_sizes.sh
@@ -5,10 +5,13 @@
 #cp -r is used to make four copies of pics_normal and names them pics_large, pics_medium, pics_small, pics_micro respectively
 #directory is changed and files are renamed
 
+
+#verbose check
 if [ "$1" == "1" ]; then v=1; fi
 
 if [ $v ]; then echo "creating folders..."; fi
 
+#make copies of normal and rename
 cp -r pics_normal pics_large
 if [ $v ]; then echo "large folder created!"; fi
 
@@ -19,43 +22,48 @@ cp -r pics_normal pics_small
 if [ $v ]; then echo "small folder created!"; fi
 
 cp -r pics_normal pics_micro
-if [ $v ]; then echo "micro folder created!"; fi
+if [ $v ]; then echo "micro folder created!\n"; fi
 
-
-if [ $v ]; then echo "resizing normal_"; fi
-cd pics_large
+#resize in loop
+if [ $v ]; then echo "resizing normal_..."; fi
+cd pics_normal
 for file in *.JPG
 do
     convert "$file" -resize 240x240 "$file"
+done
+
+if [ $v ]; then echo "resizing and appending large_..."; fi
+cd ../pics_large
+for file in *.JPG
+do
+    convert "$file" -resize 700x700 "$file"
     mv "$file" "large_$file"
 done
 
-if [ $v ]; then echo "appending large_..."; fi
-cd pics_large
-for file in *.JPG
-do
-    convert "$file" -resize 700x700 "large_$file"
-done
-
-if [ $v ]; then echo "appending medium_..."; fi
+if [ $v ]; then echo "resizing and appending medium_..."; fi
 cd ../pics_medium
 for file in *.JPG
 do
-    convert "$file" -resize 350x350 "medium_$file"
+    convert "$file" -resize 350x350 "$file"
+    mv "$file" "medium_$file"
+
 done
 
-if [ $v ]; then echo "appending small_..."; fi
+if [ $v ]; then echo "resizing and appending small_..."; fi
 cd ../pics_small
 for file in *.JPG
 do
-    convert "$file" -resize 175x175 "small_$file"
+    convert "$file" -resize 175x175 "$file"
+    mv "$file" "small_$file"
+
 done
 
-if [ $v ]; then echo "appending micro_..."; fi
+if [ $v ]; then echo "resizing and appending micro_..."; fi
 cd ../pics_micro
 for file in *.JPG
 do
-    convert "$file" -resize 25x25 "micro_$file"
+    convert "$file" -resize 25x25 "$file"
+    mv "$file" "micro_$file"
 done
 
 if [ $v ]; then echo "-rename script exit\n"; fi
